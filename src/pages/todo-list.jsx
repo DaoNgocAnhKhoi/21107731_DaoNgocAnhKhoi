9d58bc81461d3f7504bb2c8261ffaa865ccccc91
DaoNgocAnhKhoi <daongocanhkhoi@gmail.com>
2024-11-02 13:22:06 +0700

    cau 1 , cau 2a

--- src/pages/todo-list.jsx
+++ src/pages/todo-list.jsx
@@ -9,6 +9,7 @@ const TodoList = ({ route }) => {
     { id: '1', title: 'Task 1', completed: false },
     { id: '2', title: 'Task 2', completed: false },
     { id: '3', title: 'Task 3', completed: false },
+    { id: '4', title: 'Task 4', completed: true }
   ]);
 
   const handleToggleComplete = (id) => {
